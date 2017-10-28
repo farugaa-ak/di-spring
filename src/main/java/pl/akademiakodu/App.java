e2980eff90cac01a668d7be383c19551c1fa0092
Artur Faruga <farugaa.training@gmail.com>
2017-10-28 10:05:19 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/App.java
@@ -0,0 +1,13 @@
+package pl.akademiakodu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
